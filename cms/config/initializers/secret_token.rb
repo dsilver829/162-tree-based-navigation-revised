fd3293b75aaa9a1381c6fedc6ffc91e3837885b3
David Silver <dsilver@candidatemetrics.com>
2013-02-04 12:00:43 -0800

    setting up

---
+++ cms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = 'd812462ffa50bda17f13b5cba2cf52bea10f719cd576b1cbfdc261bb3ee6b9c17b3a05be6f0f4d5b792e07c8e63f7e2ae2e9329cd3201b592ef998ac336d6ee4'
