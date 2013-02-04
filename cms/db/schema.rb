56a492dec3a59af66f428c35bde59841c0c92f78
David Silver <dsilver@candidatemetrics.com>
2013-02-04 12:25:18 -0800

    enable pages with ancestry

--- cms/db/schema.rb
+++ cms/db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130204200357) do
+
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.string   "ancestry"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "pages", ["ancestry"], :name => "index_pages_on_ancestry"
 
 end
